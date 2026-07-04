183a7f6fecfc6146db479c951f720cd8e87d5f01
gustavoarroz22-boop <gustavoarroz22-boop@users.noreply.github.com>
2026-07-04 15:02:45 +0200

    Update base URL for new repository grujic

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/grujic-construction/',
+  base: '/grujic/',
   plugins: [
     tailwindcss(),
     react(),
